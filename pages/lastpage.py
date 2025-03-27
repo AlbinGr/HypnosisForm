ed3b6daf8ed923ea796289cb79868368191f2e6f
Albin Grataloup <albin.grataloup@bfh.ch>
2025-03-27 11:51:43 +0100

    final page

--- pages/lastpage.py
+++ pages/lastpage.py
@@ -1,3 +1,9 @@
 import streamlit as st
 
-st.title("Congratulations, you have completed the questionnaire!")
+st.title("Merci pour votre participation !")
+
+st.write(""" Quelques mots sur les enjeux de l'étude.
+Outre le contenu linguistique des suggestions hypnotiques (c-à-d. les mots utilisés et leur sens), nous émettons l'hypothèse que les éléments paralinguistiques (c-à-d. les modulations de la voix, le débit de la parole) jouent un rôle capital dans l'induction de la transe hypnotique.
+Cette étude visait à mieux comprendre l’influence de la voix et de la parole de l’hypnothérapeute en s’intéressant à la perception des auditeurs.""")
+
+st.write("""Si vous avez d'autres commentaires et questions, vous pouvez nous contacter à l'adresse suivante : leila.lorenzini@ulb.be ou alice.lienard@ulb.be. Votre adresse email ne pourra pas être liée à vos réponses au questionnaire en ligne. """)
